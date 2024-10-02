79673f41b35b84d859a547820df80fb778cbde1b
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2024-10-02 20:42:23 +0200

    init header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { AsyncPipe, NgIf } from "@angular/common";
+import { RouterLink } from "@angular/router";
+import { AuthService } from "../../services/auth.service";
+import { Observable, of } from "rxjs";
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+    imports: [
+        NgIf,
+        RouterLink,
+        AsyncPipe
+    ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent implements OnInit {
+    protected loggedInUser: Observable<string | null> = of(null);
+
+    constructor(private authService: AuthService) { }
+
+    ngOnInit() {
+        this.loggedInUser = this.authService.user
+    }
+}
