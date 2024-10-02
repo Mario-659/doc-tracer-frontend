faef3cd030c765cec13953a6cb1816ca48605d8f
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2024-10-02 21:56:22 +0200

    implement logout route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { LoginComponent } from './components/login/login.component'
 import { DashboardComponent } from './components/dashboard/dashboard.component'
 import { authGuard } from './guards/auth.guard'
 import { RegistrationComponent } from './components/registration/registration.component'
+import { LogoutComponent } from './components/logout/logout.component'
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
+    { path: 'logout', component: LogoutComponent },
     { path: 'register', component: RegistrationComponent },
     {
         path: 'dashboard',
---
+++ src/app/components/logout/logout.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core'
+import { NotificationService } from '../../services/notification.service'
+import { AuthService } from '../../services/auth.service'
+import { Router } from '@angular/router'
+
+@Component({
+  selector: 'app-logout',
+  standalone: true,
+  imports: [],
+  templateUrl: './logout.component.html',
+  styleUrl: './logout.component.scss'
+})
+export class LogoutComponent implements OnInit {
+
+    constructor(
+        private notificationService: NotificationService,
+        private authService: AuthService,
+        private router: Router
+    ) {}
+
+    ngOnInit() {
+        this.authService.logout()
+        this.notificationService.showNotification({message: "Logged out successfully", type: "success"})
+        this.router.navigate(['/login'])
+    }
+}
