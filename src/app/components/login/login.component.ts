8f92a9538981a81bfcb46a0e4983ccdff2ed82d6
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2024-10-05 15:30:26 +0200

    add login input validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
-import { FormsModule } from '@angular/forms'
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
 import { NgIf } from '@angular/common'
 import { AuthService } from '../../services/auth.service'
 import { HttpError } from '../../models/http-error'
@@ -10,13 +10,12 @@ import { NotificationType } from '../../models/notification'
 @Component({
     selector: 'app-login',
     standalone: true,
-    imports: [FormsModule, NgIf],
+    imports: [ReactiveFormsModule, NgIf],
     templateUrl: './login.component.html',
     styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-    username: string = ''
-    password: string = ''
+export class LoginComponent implements OnInit {
+    loginFormGroup: any;
 
     constructor(
         private router: Router,
@@ -24,8 +23,15 @@ export class LoginComponent {
         private notificationService: NotificationService
     ) {}
 
+    ngOnInit() {
+        this.loginFormGroup = new FormGroup({
+            username: new FormControl<string>('', [Validators.required]),
+            password: new FormControl<string>('', [Validators.required])
+        })
+    }
+
     login(): void {
-        this.authService.login({ username: this.username, password: this.password }).subscribe({
+        this.authService.login(this.loginFormGroup.value).subscribe({
             next: () => {
                 this.notificationService.showNotification({ message: 'Login successful', type: NotificationType.success });
                 this.router.navigate(['/dashboard'])
