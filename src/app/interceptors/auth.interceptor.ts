72e3abae5e003539e855b05dbef1e1e24051c455
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2024-10-19 15:23:14 +0200

    fix authorization

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -10,7 +10,7 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     }
 
     const authReq = req.clone({
-        headers: req.headers.set('Authorization', authToken),
+        headers: req.headers.set('Authorization', `Bearer ${authToken}`),
     })
     return next(authReq)
 }
