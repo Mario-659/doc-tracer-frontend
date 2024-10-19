be3eda157e7c154712c028a14f78810eaf935b07
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2024-10-19 23:46:56 +0200

    send update spectrum request

--- src/app/components/edit-spectrum/edit-spectrum.component.ts
+++ src/app/components/edit-spectrum/edit-spectrum.component.ts
@@ -22,9 +22,10 @@ import { AppNotification, NotificationType } from '../../models/notification'
 })
 export class EditSpectrumComponent implements OnInit {
     spectrumForm: FormGroup
-    devices: Device[] | undefined
+    devices: Device[] = []
     spectrumTypes: SpectrumType[] | undefined
     spectrumId: number = -1
+    originalSpectrum: any
 
     constructor(
         private notificationService: NotificationService,
@@ -39,27 +40,25 @@ export class EditSpectrumComponent implements OnInit {
             sampleId: ['', Validators.required],
             createdBy: ['', Validators.required],
             measurementDate: ['', Validators.required],
-            createdAt: ['', Validators.required],
-            updatedAt: ['', Validators.required],
         })
     }
 
     ngOnInit(): void {
         // TODO forkjoin these requests
-        this.route.paramMap.subscribe(
-            params => {
-                this.spectrumId = Number(params.get('id'))
-                this.loadSpectrumDetails(this.spectrumId)
-            },
-        )
-        this.loadDeviceNames()
-        this.loadSpectrumTypes()
+        // TODO add spectrum json edit
+        this.route.paramMap.subscribe(params => {
+            this.spectrumId = Number(params.get('id'));
+            this.loadSpectrumDetails(this.spectrumId);
+        });
+        this.loadDeviceNames();
+        this.loadSpectrumTypes();
     }
 
     loadSpectrumDetails(id: number): void {
         this.dataService.getSpectrum(id).subscribe(spectrum => {
-            const measurementDate = spectrum.measurementDate ? spectrum.measurementDate.split('T')[0] : '';
+            this.originalSpectrum = spectrum
 
+            const measurementDate = spectrum.measurementDate ? spectrum.measurementDate.split('T')[0] : ''
             this.spectrumForm.patchValue({
                 ...spectrum,
                 measurementDate: measurementDate
@@ -71,27 +70,56 @@ export class EditSpectrumComponent implements OnInit {
         this.dataService.getDeviceNames().subscribe(devices => this.devices = devices)
     }
 
-
     loadSpectrumTypes(): void {
-        this.dataService.getSpectrumTypes().subscribe(spectraTypes => this.spectrumTypes = spectraTypes)
+        this.dataService.getSpectrumTypes().subscribe(spectraTypes => this.spectrumTypes = spectraTypes);
     }
 
     save(): void {
-        if (!this.spectrumForm.valid) return
+        if (!this.spectrumForm.valid) return;
 
-        const updateRequest: SpectrumUpdateRequest = {
+        const formValues = this.spectrumForm.value;
 
+        const updateRequest: SpectrumUpdateRequest = this.getChangedFields(formValues, this.originalSpectrum);
+
+        if (Object.keys(updateRequest).length === 0) {
+            this.notificationService.showNotification(new AppNotification(`No changes detected`, NotificationType.info));
+            return;
         }
 
         this.dataService.updateSpectrum(this.spectrumId, updateRequest).subscribe(() => {
-            this.notificationService.showNotification(new AppNotification(`Spectrum with id ${this.spectrumId} has been updated`, NotificationType.success))
-            this.router.navigate(['/spectrum', this.spectrumId]);
+            this.notificationService.showNotification(new AppNotification(`Spectrum with id ${this.spectrumId} has been updated`, NotificationType.success));
+            this.goSpectrumDetails()
         });
+    }
 
+    getChangedFields(formValues: any, originalSpectrum: any): SpectrumUpdateRequest {
+        const updateRequest: SpectrumUpdateRequest = {};
+
+        if (formValues.spectrumTypeName !== originalSpectrum.spectrumTypeName) {
+            updateRequest.spectrumType = formValues.spectrumTypeName;
+        }
+
+        if (formValues.deviceName !== originalSpectrum.deviceName) {
+            updateRequest.deviceId = this.devices.find(d => d.name === formValues.deviceName)?.id
+        }
+
+        if (formValues.sampleId !== originalSpectrum.sampleId) {
+            updateRequest.sampleId = formValues.sampleId;
+        }
+
+        if (formValues.createdBy !== originalSpectrum.createdBy) {
+            updateRequest.username = formValues.createdBy;
+        }
+
+        if (formValues.measurementDate !== originalSpectrum.measurementDate.split('T')[0]) {
+            updateRequest.measurementDate = new Date(formValues.measurementDate).toISOString();
+        }
+
+        return updateRequest;
     }
 
     goSpectrumDetails() {
-        this.router.navigate([`/spectra/${this.spectrumId}`])
+        this.router.navigate([`/spectra/${this.spectrumId}`]);
     }
 }
 
