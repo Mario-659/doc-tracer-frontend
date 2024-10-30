080ca402102ea96432024b7f1c0a74435bbb77a4
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2024-10-30 10:58:05 +0100

    add logged in username

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router'
-import { NgClass, NgForOf, NgIf, NgOptimizedImage } from '@angular/common'
+import { AsyncPipe, NgClass, NgForOf, NgIf, NgOptimizedImage } from '@angular/common'
+import { AuthService } from '../../services/auth.service'
+import { Observable } from 'rxjs'
 
 @Component({
   selector: 'app-sidebar',
@@ -12,12 +14,14 @@ import { NgClass, NgForOf, NgIf, NgOptimizedImage } from '@angular/common'
         NgForOf,
         NgIf,
         NgOptimizedImage,
+        AsyncPipe,
     ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
     isSidebarExpanded = false;
+    protected loggedInUser$: Observable<string | null>
     navItems = [
         { label: 'Dashboard', icon: 'dashboard', link: '/dashboard' },
         { label: 'Spectra', icon: 'query_stats', link: '/spectra' },
@@ -25,6 +29,10 @@ export class SidebarComponent {
         { label: 'Samples', icon: 'labs', link: '/samples' },
         { label: 'Measuring Devices', icon: 'tablet_camera', link: '/measuring-devices' }
     ];
+
+    constructor(private authService: AuthService) {
+        this.loggedInUser$ = this.authService.loggedInUser$
+    }
 
     toggleSidebar() {
         this.isSidebarExpanded = !this.isSidebarExpanded
