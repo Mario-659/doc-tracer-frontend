705d4dec50689834486566d4dc7b77c5338fe4e6
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2024-11-15 15:58:45 +0100

    revert custom ag grid theme

--- src/app/components/app-grid/app-grid.component.ts
+++ src/app/components/app-grid/app-grid.component.ts
@@ -3,8 +3,6 @@ import { ColDef } from 'ag-grid-community'
 import { Observable } from 'rxjs'
 import { AsyncPipe, NgIf } from '@angular/common'
 import { AgGridAngular } from 'ag-grid-angular'
-import { iconSetQuartzLight, themeQuartz } from '@ag-grid-community/theming'
-import { GridOptions } from '@ag-grid-community/core'
 
 @Component({
     selector: 'app-grid',
@@ -18,32 +16,5 @@ export class AppGridComponent {
     @Input() $rowData: Observable<any> | undefined
 
     constructor() {
-    }
-
-
-    myTheme = themeQuartz
-        .withPart(iconSetQuartzLight)
-        .withParams({
-            backgroundColor: '#ffffff',
-            browserColorScheme: 'light',
-            columnBorder: false,
-            fontFamily: {
-                googleFont: 'Arial',
-            },
-            foregroundColor: 'rgb(46, 55, 66)',
-            headerBackgroundColor: '#F9FAFB',
-            headerFontSize: 14,
-            headerFontWeight: 600,
-            headerTextColor: '#919191',
-            oddRowBackgroundColor: '#F9FAFB',
-            rowBorder: false,
-            sidePanelBorder: false,
-            spacing: 8,
-            wrapperBorder: false,
-            wrapperBorderRadius: 0,
-        })
-
-    gridOptions = {
-        'theme': this.myTheme
     }
 }
