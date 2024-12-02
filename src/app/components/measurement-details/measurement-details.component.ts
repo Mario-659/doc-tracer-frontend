7a680ab82e565403f9c2d7e830da432b6cf13d3a
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2024-12-02 15:44:27 +0100

    add remove measurement

--- src/app/components/measurement-details/measurement-details.component.ts
+++ src/app/components/measurement-details/measurement-details.component.ts
@@ -7,6 +7,9 @@ import { AsyncPipe, DatePipe, NgIf } from '@angular/common'
 import { AgGridAngular } from 'ag-grid-angular'
 import { Sample } from '../../models/api/sample'
 import { ColDef } from 'ag-grid-community'
+import { Modal } from 'bootstrap'
+import { AppNotification, NotificationType } from '../../models/notification'
+import { NotificationService } from '../../services/notification.service'
 
 @Component({
     selector: 'app-measurement-details',
@@ -18,11 +21,13 @@ import { ColDef } from 'ag-grid-community'
 export class MeasurementDetailsComponent implements OnInit {
     measurement$: Observable<Measurement | undefined> | undefined
     samples$: Observable<Sample[] | undefined> | undefined
+    confirmationModal: any;
 
     constructor(
         private route: ActivatedRoute,
         private dataService: DataService,
-        private router: Router
+        private router: Router,
+        private notificationService: NotificationService
     ) {}
 
     ngOnInit(): void {
@@ -95,5 +100,31 @@ export class MeasurementDetailsComponent implements OnInit {
 
     goToEditMeasurement(id: number) {
         this.router.navigate(['/measurements', id, 'edit'])
+    }
+
+    deleteMeasurement(id: number) {
+        const confirmationModalElement = document.getElementById('confirmationModal')
+        if (confirmationModalElement) {
+            this.confirmationModal = new Modal(confirmationModalElement)
+            this.confirmationModal.show()
+        }
+    }
+
+
+    confirmDelete(id: number): void {
+        this.dataService.deleteMeasurement(id).subscribe({
+            next: () => {
+                this.notificationService.showNotification(
+                    new AppNotification(`Measurement with ID ${id} deleted successfully`, NotificationType.success)
+                )
+                this.closeModal()
+                this.router.navigate(['/measurements'])
+            },
+        })
+    }
+
+
+    closeModal() {
+        this.confirmationModal.hide()
     }
 }
